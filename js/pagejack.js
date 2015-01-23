c72d5718b9a39e7899ddc6382de407e450feab65
Kenta Takanashi <takanashi@motionpicture.jp>
2015-01-23 16:52:52 +0900

    背景画像修正

---
+++ js/pagejack.js
@@ -0,0 +1,5 @@
+$(function() {
+    $("#container").wrapInner('<div id="jackWrap"></div>');
+    $("#jackWrap").append('<div id="jackLeft"><a href="http://www.foxmovies-jp.com/exodus/" target="_blank"><img src="/images/img_pagejack_left.jpg"></a></div>');
+    $("#jackWrap").prepend('<div id="jackRight"><a href="http://www.foxmovies-jp.com/exodus/" target="_blank"><img src="/images/img_pagejack_right.jpg"></a></div>');
+});
