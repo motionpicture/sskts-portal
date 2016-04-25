9bf4bf5a83feca4bbb1c5f32a29e9ed2245e6fd2
akito hataguchi <hataguchi@motionpicture.jp>
2016-04-25 18:15:07 +0900

    SASAKI-92 各劇場ページタグ埋込

--- lib/create.php
+++ lib/create.php
@@ -1152,24 +1152,7 @@ var yahoo_retargeting_label = '';
 
 <!--/gaie-YDN-コンバージョンタグ-->
 
-<!--gaie-FB計測タグ-->
 
-<!-- Facebook Pixel Code -->
-<script>
-!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
-n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
-document,'script','https://connect.facebook.net/en_US/fbevents.js');
-
-fbq('init', '212446625793321');
-fbq('track', "PageView");</script>
-<noscript><img height="1" width="1" style="display:none"
-src="https://www.facebook.com/tr?id=212446625793321&ev=PageView&noscript=1"
-/></noscript>
-<!-- End Facebook Pixel Code -->
-
-<!--/gaie-FB計測タグ-->
 
 
 EOL;
