4d2a285b3deb7799de58ee0ba97cff578d0ddef9
Shun <shun@motionpicture.jp>
2016-06-24 16:00:06 +0900

    ↓のコミットミス直し

--- mailmagazine/txt/index.php
+++ mailmagazine/txt/index.php
@@ -8,7 +8,20 @@ ob_start();
 <meta http-equiv="Content-Type" content="text/html; charset=Shift_JIS" />
 <title>シネマサンシャインメールマガジン簡易テキスト版</title>
 <script>
-if(window.XMLHttpRequest){
+var ua = navigator.userAgent.toLowerCase();
+if(ua.indexOf('docomo') !== -1
+|| ua.indexOf('up.browser') !== -1
+|| ua.indexOf('softbank') !== -1
+|| ua.indexOf('vodafone') !== -1
+|| ua.indexOf('j-phone') !== -1
+|| ua.indexOf('kddi') !== -1
+|| ua.indexOf('willcom') !== -1
+|| !window.XMLHttpRequest){
+  window.isMobile = true;
+}else{
+  window.isMobile = false; 
+}
+if(!window.isMobile){
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -22,15 +35,22 @@ if(window.XMLHttpRequest){
 <noscript>
 <img src="<?php echo mobileGoogleAnalyticsGetImageUrl('MO-8383230-68'); ?>" />
 </noscript>
+<script>
+if(window.isMobile){
+  var imgGA = document.createElement('img');
+  imgGA.src = "<?php echo mobileGoogleAnalyticsGetImageUrl('MO-8383230-68'); ?>";
+  document.body.insertBefore(imgGA,document.getElementsByTagName('noscript')[0]);
+}
+</script>
 <?php
-$issue = trim(htmlspecialchars(str_replace('/','',$_GET['issue']),ENT_NOQUOTES));
+$issue = trim(htmlspecialchars($_GET['issue'],ENT_NOQUOTES));
 if($issue){
   $html = file_get_contents('./'.$issue.'/text.html');
-}
-if($html){
-  echo $html;
-}else{
-  echo 'NOT FOUND';
+  if($html){
+    echo $html;
+  }else{
+    echo 'NOT FOUND';
+  }
 }
 $output = ob_get_contents();
 ob_end_clean();
