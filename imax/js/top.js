2d2429c91387ac500ac7b5a6e42b704391b654de
akito hataguchi <hataguchi@motionpicture.jp>
2016-08-31 12:24:34 +0900

    IMAX動画js修正

--- imax/js/top.js
+++ imax/js/top.js
@@ -19,27 +19,33 @@
         tag.src = "https://www.youtube.com/iframe_api";
         var firstScriptTag = document.getElementsByTagName('script')[0];
         firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+        
         $(function(){
-            //showVideo();
+            //非表示の場合下の行をコメントアウト
+            showVideo('HqHVkLe0hdA');//ID設定
         });
     }
-    function showVideo() {
+
+    function showVideo(_videoId) {
         onYouTubeIframeAPIReady = function() {
             dom.show();
             ytPlayer = new YT.Player('youtube', youtubeObj);
         };
+
         var onPlayerStateChange = function(e) {
             if (e.data == YT.PlayerState.ENDED) {
             setTimeout(youtubeClose, 500);
             }
         }
+
         var onPlayerReady = function() {
             ytPlayer.setPlaybackQuality('hd720');
         }
+
         var youtubeObj = {
             width: 960,
             height: 540,
-            videoId: '32I66B-a478',//ID設定
+            videoId: _videoId,
             playerVars: {
             rel: 0,
             autoplay: 1
@@ -49,27 +55,34 @@
             'onStateChange': onPlayerStateChange
             }
         };
+
         var dom = $('<div class="modal-cover">\
                         <div class="modal">\
                         <div id="youtube"></div>\
                         <div class="close_btn"><a href="#"><img src="/imax/images/common/close_btn.png"></a></div>\
                         </div>\
                     </div>');
+
         var youtubeClose = function() {
             dom.find('.modal').fadeOut(200, function(){dom.remove()});
         };
+
         $('#wrapper').append(dom);
+
         dom.find('#youtube').css({
             marginTop: (youtubeObj.height / 2 * -1) + 'px',
             marginLeft: (youtubeObj.width / 2 * -1) + 'px'
         });
+
         dom.find('.close_btn').css({
             marginTop: (youtubeObj.height / 2 * -1) + 'px'
         });
+
         $('.modal-cover').live('click', function(e) {
             e.preventDefault();
             youtubeClose();
         });
+        
         dom.find('.close_btn a').live('click', function(e) {
             e.preventDefault();
             youtubeClose();
