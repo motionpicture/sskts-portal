8b0fb15da1ad69bcbe4aded08cd64502054db91a
Atsushi Okui <okui@motionpicture.jp>
2016-12-28 18:04:42 +0900

    SASAKI_TICKET-35 Azureにサーバ移管 ・CMSのconstを各環境に対応 ・CMSのDB設定を更新（各環境用の接続情報、ドライバをmysqliに変更）

--- cine_cms/app/config/database.php
+++ cine_cms/app/config/database.php
@@ -1,8 +1,10 @@
 <?php
 class DATABASE_CONFIG {
 
-	var $default = array(
-		'driver' => 'mysql',
+	var $default = array();
+
+    var $prod = array(
+		'driver' => 'mysqli',
 		'persistent' => false,
 		'host' => 'localhost',
 		'login' => 'cinesun_cms',
@@ -10,5 +12,38 @@ class DATABASE_CONFIG {
 		'database' => 'cinema_cms',
 		'encoding' => 'utf8'
 	);
+
+    // 自動テスト用
+    var $test = array();
+
+    var $stg = array(
+		'driver' => 'mysqli',
+		'persistent' => false,
+		'host' => 'ja-cdbr-azure-east-a.cloudapp.net',
+		'login' => 'b68a3a830cc797',
+		'password' => 'c9e1bde9',
+		'database' => 'testsasakidb',
+		'encoding' => 'utf8'
+	);
+
+    var $dev = array(
+		'driver' => 'mysqli',
+		'persistent' => false,
+		'host' => 'ja-cdbr-azure-east-a.cloudapp.net',
+		'login' => 'b68a3a830cc797',
+		'password' => 'c9e1bde9',
+		'database' => 'testsasakidb',
+		'encoding' => 'utf8'
+	);
+
+    function __construct() {
+        if (APP_ENV == 'prod') {
+            $this->default = $this->prod;
+        } else if (APP_ENV == 'stg') {
+            $this->default = $this->stg;
+        } else {
+            $this->default = $this->dev;
+        }
+    }
 }
 ?>
