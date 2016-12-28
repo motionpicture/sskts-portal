80ae756e823e55d29c65f0616493fd2ef8da9aad
Atsushi Okui <okui@motionpicture.jp>
2016-12-28 19:34:23 +0900

    SASAKI_TICKET-17 [鹿児島追加]ポータルサイト改修　一次リリース スマホサイトに鹿児島追加（池袋をコピー） 鹿児島ページへのリンクを追加

---
+++ sp/theater/kagoshima/concession/index.php
@@ -0,0 +1,391 @@
+<?php
+include("../../../../lib/require.php");
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<?php getSmartHeadInclude(); ?>
+	<link rel="stylesheet" type="text/css" href="../../../css/concession.css">
+</head>
+<body>
+	<?php getSmartHeader(); ?>
+	<?php getSmartPankuzu(); ?>
+	<div class="basebox2_ptrl">
+		
+        <!-- 池袋SPコンセッション -->
+        
+        <!-- table -->
+		<table cellpadding="0" cellspacing="0" class="theater_u">
+			<thead>
+				<tr>
+					<th colspan="2" class="table_line_01"></th>
+				</tr>
+			</thead>
+		</table>
+		<table cellpadding="0" cellspacing="0" class="theater_t">
+			<tbody>
+				<tr class="first-child">
+					<td colspan="2" class="table_top_01">ソフトドリンク</td>
+				</tr>
+			</tbody>
+		</table>
+
+		<div class="table_wrapper regulation02">
+			<table cellpadding="0" cellspacing="0" class="theater_u">
+				<tbody>
+					<tr>
+						<td class="admission_tl">コーラ・ジンジャーエール・ファンタメロン・カルピス・カルピスソーダ・アイスティー・ウーロン茶</td>
+						<td class="admission_tr">Ｓサイズ 250円<br />Ｍサイズ 350円<br />Lサイズ 450円</td>
+					</tr>
+					<tr>
+						<td colspan="2" class="table_line_02"></td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+		<!-- table end -->
+
+		<!-- table -->
+		<table cellpadding="0" cellspacing="0" class="theater_u">
+			<thead>
+				<tr>
+					<th colspan="2" class="table_line_01"></th>
+				</tr>
+			</thead>
+		</table>
+		<table cellpadding="0" cellspacing="0" class="theater_t">
+			<tbody>
+				<tr class="first-child">
+					<td colspan="2" class="table_top_01">コーヒー</td>
+				</tr>
+			</tbody>
+		</table>
+
+		<div class="table_wrapper regulation02">
+			<table cellpadding="0" cellspacing="0" class="theater_u">
+				<tbody>
+					<tr>
+						<td class="admission_tl">ホットコーヒー・アイスコーヒー</td>
+						<td class="admission_tr">350円</td>
+					</tr>
+					<tr>
+						<td colspan="2" class="table_line_02"></td>
+					</tr>
+					<tr>
+						<td class="admission_tl">カフェラテ・アイスラテ</td>
+						<td class="admission_tr">380円</td>
+					</tr>
+					<tr>
+						<td colspan="2" class="table_line_02"></td>
+					</tr>
+					<tr>
+						<td class="admission_tl">カフェモカ・アイスモカ</td>
+						<td class="admission_tr">380円</td>
+					</tr>
+					<tr>
+						<td colspan="2" class="table_line_02"></td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+		<!-- table end -->
+
+		<!-- table -->
+		<table cellpadding="0" cellspacing="0" class="theater_u">
+			<thead>
+				<tr>
+					<th colspan="2" class="table_line_01"></th>
+				</tr>
+			</thead>
+		</table>
+		<table cellpadding="0" cellspacing="0" class="theater_t">
+			<tbody>
+				<tr class="first-child">
+					<td colspan="2" class="table_top_01">アルコール</td>
+				</tr>
+			</tbody>
+		</table>
+
+		<div class="table_wrapper regulation02">
+			<table cellpadding="0" cellspacing="0" class="theater_u">
+				<tbody>
+					<tr>
+						<td class="admission_tl">缶ビール</td>
+						<td class="admission_tr">450円</td>
+					</tr>
+					<tr>
+						<td colspan="2" class="table_line_02"></td>
+					</tr>
+					<tr>
+						<td class="admission_tl">缶チューハイ</td>
+						<td class="admission_tr">400円</td>
+					</tr>
+					<tr>
+						<td colspan="2" class="table_line_02"></td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+		<!-- table end -->
+
+		<!-- table -->
+		<table cellpadding="0" cellspacing="0" class="theater_u">
+			<thead>
+				<tr>
+					<th colspan="2" class="table_line_01"></th>
+				</tr>
+			</thead>
+		</table>
+		<table cellpadding="0" cellspacing="0" class="theater_t">
+			<tbody>
+				<tr class="first-child">
+					<td colspan="2" class="table_top_01">定番ドリンク</td>
+				</tr>
+			</tbody>
+		</table>
+
+		<div class="table_wrapper regulation02">
+			<table cellpadding="0" cellspacing="0" class="theater_u">
+				<tbody>
+					<tr>
+						<td class="admission_tl">ペットボトル<br /><span class="notice">ミネラルフォーター・コーラZERO・爽健美茶・綾鷹上煎茶・QOOオレンジ</span></td>
+						<td class="admission_tr">210円</td>
+					</tr>
+					<tr>
+						<td colspan="2" class="table_line_02"></td>
+					</tr>
+                    <tr>
+						<td colspan="2" class="admission_tl regulation04">アイス</td>
+					</tr>
+					<tr>
+						<td class="admission_tl regulation03"><span class="notice">ココア</span></td>
+						<td class="admission_tr regulation03">350円</td>
+					</tr>
+					<tr>
+						<td class="admission_tl regulation03"><span class="notice">抹茶オレ</span></td>
+						<td class="admission_tr regulation03">320円</td>
+					</tr>
+					<tr>
+						<td colspan="2" class="table_line_02"></td>
+					</tr>
+                    <tr>
+						<td colspan="2" class="admission_tl regulation04">ホット</td>
+						
+					</tr>
+					<tr>
+						<td class="admission_tl regulation03 regulation04"><span class="notice">ココア</span></td>
+						<td class="admission_tr regulation03 regulation04">350円</td>
+					</tr>
+                    <tr>
+						<td class="admission_tl regulation03"><span class="notice">抹茶オレ</span></td>
+						<td class="admission_tr regulation03">320円</td>
+					</tr>
+					<tr>
+						<td colspan="2" class="table_line_02"></td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+		<!-- table end -->
+        
+        
+
+		<!-- table -->
+		<table cellpadding="0" cellspacing="0" class="theater_u">
+			<thead>
+				<tr>
+					<th colspan="2" class="table_line_01"></th>
+				</tr>
+			</thead>
+		</table>
+		<table cellpadding="0" cellspacing="0" class="theater_t">
+			<tbody>
+				<tr class="first-child">
+					<td colspan="2" class="table_top_01">ポップコーン</td>
+				</tr>
+			</tbody>
+		</table>
+
+		<div class="table_wrapper regulation02">
+			<table cellpadding="0" cellspacing="0" class="theater_u">
+				<tbody>
+					<tr>
+						<td class="admission_tl">塩</td>
+						<td class="admission_tr">Mサイズ 300円<br />Lサイズ 500円</td>
+					</tr>
+					<tr>
+						<td colspan="2" class="table_line_02"></td>
+					</tr>
+					<tr>
+						<td class="admission_tl">キャラメル</td>
+						<td class="admission_tr">Mサイズ 400円<br />Lサイズ 700円</td>
+					</tr>
+					<tr>
+						<td colspan="2" class="table_line_02"></td>
+					</tr>
+					<tr>
+						<td class="admission_tl">塩/キャラメル</td>
+						<td class="admission_tr">ハーフ&ハーフ 600円</td>
+					</tr>
+					<tr>
+						<td colspan="2" class="table_line_02"></td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+		<!-- table end -->
+
+		<!-- table -->
+		<table cellpadding="0" cellspacing="0" class="theater_u">
+			<thead>
+				<tr>
+					<th colspan="2" class="table_line_01"></th>
+				</tr>
+			</thead>
+		</table>
+		<table cellpadding="0" cellspacing="0" class="theater_t">
+			<tbody>
+				<tr class="first-child">
+					<td colspan="2" class="table_top_01">アイスクリーム</td>
+				</tr>
+			</tbody>
+		</table>
+
+		<div class="table_wrapper regulation02">
+			<table cellpadding="0" cellspacing="0" class="theater_u">
+				<tbody>
+					<tr>
+						<td class="admission_tl ">アイスクレープ</td>
+						<td class="admission_tr">360円</td>
+					</tr>
+					<tr>
+						<td colspan="2" class="table_line_02"></td>
+					</tr>
+					<tr>
+						<td class="admission_tl">ミニアイスクレープ</td>
+						<td class="admission_tr">310円</td>
+					</tr>
+					<tr>
+						<td colspan="2" class="table_line_02"></td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+		<!-- table end -->
+
+		<!-- table -->
+		<table cellpadding="0" cellspacing="0" class="theater_u">
+			<thead>
+				<tr>
+					<th colspan="2" class="table_line_01"></th>
+				</tr>
+			</thead>
+		</table>
+		<table cellpadding="0" cellspacing="0" class="theater_t">
+			<tbody>
+				<tr class="first-child">
+					<td colspan="2" class="table_top_01">定番フード</td>
+				</tr>
+			</tbody>
+		</table>
+
+		<div class="table_wrapper regulation02">
+		<table cellpadding="0" cellspacing="0" class="theater_u">
+			<tbody>
+				<tr>
+					<td class="admission_tl">ベルギーワッフル</td>
+					<td class="admission_tr">450円</td>
+				</tr>
+				<tr>
+					<td colspan="2" class="table_line_02"></td>
+				</tr>
+			</tbody>
+		</table>
+		</div>
+		<!-- table end -->
+
+		<!-- table -->
+		<table cellpadding="0" cellspacing="0" class="theater_u">
+			<thead>
+				<tr>
+					<th colspan="2" class="table_line_01"></th>
+				</tr>
+			</thead>
+		</table>
+		<table cellpadding="0" cellspacing="0" class="theater_t">
+			<tbody>
+				<tr class="first-child">
+					<td colspan="2" class="table_top_01">セット</td>
+				</tr>
+			</tbody>
+		</table>
+
+		<div class="table_wrapper">
+			<table cellpadding="0" cellspacing="0" class="theater_u">
+				<tbody>
+					<tr>
+						<td colspan="2" class="admission_tl regulation04">ポップコーン塩</td>
+					</tr>
+					<tr>
+						<td class="admission_tl regulation03 regulation04"><span class="notice">ペア(ポップコーンL×1＋ドリンクM×2)</span></td>
+						<td class="admission_tr regulation03 regulation04">1,100円</td>
+                    </tr>
+                    <tr>
+						<td class="admission_tl regulation03 regulation04"><span class="notice">ドリンクM or コーヒーセット</span></td>
+						<td class="admission_tr regulation03 regulation04">600円</td>
+                    </tr>
+                    <tr>
+						<td class="admission_tl regulation03"><span class="notice">カフェラテセット</span></td>
+						<td class="admission_tr regulation03">630円</td>
+                    </tr>
+                    <tr>
+						<td class="admission_tl regulation03"><span class="notice">LLセット(ポップコーンL＋ドリンクL)</span></td>
+						<td class="admission_tr regulation03">850円</td>
+                    </tr>
+					</tr>
+						<td colspan="2" class="table_line_02"></td>
+					</tr>
+                    <tr>
+						<td colspan="2" class="admission_tl regulation04">ポップコーンキャラメル</td>
+					</tr>
+					<tr>
+						<td class="admission_tl regulation03 regulation04"><span class="notice">ペア(ポップコーンL×1＋ドリンクM×2)</span></td>
+						<td class="admission_tr regulation03 regulation04">1,300円</td>
+                    </tr>
+                    <tr>
+						<td class="admission_tl regulation03 regulation04"><span class="notice">ドリンクM or コーヒーセット</span></td>
+						<td class="admission_tr regulation03 regulation04">700円</td>
+                    </tr>
+                    <tr>
+						<td class="admission_tl regulation03"><span class="notice">カフェラテセット</span></td>
+						<td class="admission_tr regulation03">730円</td>
+                    </tr>
+                    <tr>
+						<td class="admission_tl regulation03"><span class="notice">LLセット(ポップコーンL＋ドリンクL)</span></td>
+						<td class="admission_tr regulation03">1,050円</td>
+                    </tr>
+					<tr>
+						<td colspan="2" class="table_line_02"></td>
+					</tr>
+                    <tr>
+						<td colspan="2" class="admission_tl regulation04">ポップコーン塩・キャラメル（ハーフ&ハーフ）</td>
+					</tr>
+					<tr>
+						<td class="admission_tl regulation03"><span class="notice">ペア(ポップコーンL×1＋ドリンクM×2)</span></td>
+						<td class="admission_tr regulation03">1,200円</td>
+                    </tr>
+					<tr>
+						<td class="admission_tl regulation03"><span class="notice">LLセット(ポップコーンL＋ドリンクL)</span></td>
+						<td class="admission_tr regulation03">950円</td>
+                    </tr>
+						<td colspan="2" class="table_line_02"></td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+		<!-- table end -->
+	</div>
+
+	<?php getSmartFooter(); ?>
+</body>
+</html>
