b14f1487ee8f94517213a51ec841e6158f0c4aa0
西原吾郎 <nishihara@motionpicture.jp>
2017-01-17 17:13:29 +0900

    IMAX特設サイト動画差し替え

--- imax/js/top.js
+++ imax/js/top.js
@@ -13,7 +13,7 @@
         
         $(function(){
             //非表示の場合下の行をコメントアウト
-            var videoId = 'EKASAeOsq64';
+            var videoId = '7x1uXgKMgY4';
             showVideo(videoId);//ID設定
         });
     }
