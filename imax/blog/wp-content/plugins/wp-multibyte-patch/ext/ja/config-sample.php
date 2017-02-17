3419ac4c0b69da9cc4d453668ede2ce36caec642
Atsushi Okui <okui@motionpicture.jp>
2017-02-17 13:50:23 +0900

    SSKTS-109 [Azureにサーバ移管]IMAXオフィシャルブログをクローズ wordpressを削除

--- imax/blog/wp-content/plugins/wp-multibyte-patch/ext/ja/config-sample.php
+++
@@ -1,9 +0,0 @@
-<?php
-/* WP Multibyte Patch extension config file */
-
-// Sets the encoding for wp_mail().
-// Available options are "JIS", "UTF-8" or "auto".
-// "auto" picks  "JIS" or "UTF-8" automatically.
-$wpmp_conf['mail_mode'] = 'JIS';
-
-?>
