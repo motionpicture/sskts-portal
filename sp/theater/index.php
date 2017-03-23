d793ccffef73c8bd18da4d6c8a32fb02d7937c7b
Atsushi Okui <okui@motionpicture.jp>
2017-03-23 18:00:25 +0900

    SP劇場一覧を更新（4DX、IMAXアイコン追加）
    SSKTS-17 [鹿児島追加]ポータルサイト改修　一次リリース

--- sp/theater/index.php
+++ sp/theater/index.php
@@ -43,7 +43,7 @@ include("../../lib/require.php");
 	</ul>
     <ul class="theatre_list">
 		<li class="theatre_category"><h3>九州地区</h3></li>
-		<li class="theatre_area"><a href="./aira">姶良</a></li>
+		<li class="theatre_area"><a href="./aira">姶良<span class="area_icon"><img src="../images/theater/icon_4dx.png" width="65" alt="4dx"><img src="../images/theater/icon_imax.png" width="48" alt="IMAX"></span></a></li>
 	</ul>
 
 	<?php getSmartFooter(); ?>
