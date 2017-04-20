c960e4c3f1ceeb9b6f83f44e8a0f010b86c24803
Atsushi Okui <okui@motionpicture.jp>
2017-04-20 13:53:30 +0900

    チケッティングについて、prod以外はテストサイトのリンクに変更

--- lib/const.php
+++ lib/const.php
@@ -250,11 +250,11 @@ define ("Policy_SP_URL", GROBAL_SP_TOP_URL . "sitepolicy/");
 //スマートフォンのサイトマップページURL
 define ("SiteMap_SP_URL", GROBAL_SP_TOP_URL . "sitemap/");
 
+
 if (APP_ENV == 'prod') {
     define('TICKETING_BASE_URL', 'https://ticket-cinemasunshine.com');
 } else {
-//    define('TICKETING_BASE_URL', 'https://testssktsfrontend.azurewebsites.net');
-    define('TICKETING_BASE_URL', 'https://ticket-cinemasunshine.com'); // 一時的に本番URLを設定
+    define('TICKETING_BASE_URL', 'https://testssktsfrontend.azurewebsites.net');
 }
 
 ?>
