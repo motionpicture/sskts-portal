7add31e1384ff956813178f5450c8e3cf051aa46
Atsushi Okui <okui@motionpicture.jp>
2017-06-03 16:50:01 +0900

    仮メンテナンスページを追加 SSKTS-381
    ・内容が仮のメンテナンスページ追加
    ・メンテナンスページ表示用のWeb.config追加

---
+++ maintenance.php
@@ -0,0 +1,14 @@
+<?php
+http_response_code(503);
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>メンテナンス</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <div>メンテナンス中です</div>
+    </body>
+</html>
