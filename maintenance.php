31d870b93e2e81404b715bd9f45a8181fc0f042c
akito hataguchi <hataguchi@motionpicture.jp>
2017-06-05 18:30:26 +0900

    SSKTS-381 メンテナンスHTML作成

--- maintenance.php
+++ maintenance.php
@@ -7,8 +7,19 @@ http_response_code(503);
         <title>メンテナンス</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <style>
+            body {
+                margin: 0;
+                padding: 50px;
+            }
+            img {
+                margin: 0 auto;
+                display: block;
+                vertical-align: bottom;
+            }
+        </style>
     </head>
     <body>
-        <div>メンテナンス中です</div>
+        <img src="/images/maintenance.png" width="600" height="320" alt="メンテナンス">
     </body>
 </html>
