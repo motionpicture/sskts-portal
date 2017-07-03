856e40e95f295d38985f9b8fd8ed3c8873a744ac
sato <motionpicture34@motionpicture34-no-MacBook-Pro-2.local>
2017-07-03 16:01:28 +0900

    SSKTS-316 姶良SP_Adsense位置変更2

--- sp/theater/aira/index.php
+++ sp/theater/aira/index.php
@@ -269,25 +269,6 @@ if(!empty($_GET["pre"])) {
 	<div class="line_01"></div>
 	<!--/ライン-->
 
-
-	<!-- ↓adsense上部↓ -->
-	<!--<div class="section ptb10">
-		<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-        <!-- シネサン(SP 姶良上部) -->
-    <!--    <ins class="adsbygoogle"
-             style="display:inline-block;width:320px;height:50px"
-             data-ad-client="ca-pub-3891476404601512"
-             data-ad-slot="6783687364"></ins>
-        <script>
-        (adsbygoogle = window.adsbygoogle || []).push({});
-        </script>
-	</div>
-	<!-- ↑adsense上部↑ -->
-
-	<!--<?php
-	echo getSmartTrailer();
-	?>-->
-
 	<div class="section ptb10">
 		<?php
 			foreach($result["data"]->movie as $movie) {
