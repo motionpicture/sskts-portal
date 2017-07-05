469cb7359650ceabc1ff5064ee166c5724042514
sato <motionpicture34@motionpicture34-no-MacBook-Pro-2.local>
2017-07-05 18:20:12 +0900

    SSKTS-316 上映中作品：Adsense部分クラス追加

--- sp/showing/index.php
+++ sp/showing/index.php
@@ -47,7 +47,7 @@ include("../../lib/require.php");
 
 
 	<!-- ↓adsense上部↓ -->
-	<div class="section ptb10">
+	<div class="g_Ad_sp_content ptb10">
 		<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 		<!-- シネサン（SP上映中上部） -->
 		<ins class="adsbygoogle"
