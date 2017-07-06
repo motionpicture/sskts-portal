3a39c0b68f7fe163c405523523786bb260be00c6
沼崎一朗 <numazaki@motionpicture.jp>
2017-07-06 16:06:37 +0900

    SSKTS-316 ムビチケSPページ作成

--- sp/mvtk/index.php
+++ sp/mvtk/index.php
@@ -32,7 +32,7 @@ include("../../lib/require.php");
 			<div class="about_mvtk1">
 				<h3>ムビチケについて</h3>
 				<div>
-					<img class="mvtk_pg_logo" src="../../sp/images/mvtk/mvtk_SP_logo.gif" width="478" height="106" alt="ムビチケロゴ">
+					<img class="mvtk_pg_logo" src="../../sp/images/mvtk/mvtk_SP_logo.gif" width="239" alt="ムビチケロゴ">
 					<p class="content_text">ムビチケとは?</p>
 					<p class="Lead_text">ムビチケとは、ネットで座席指定ができる<br>映画前売券になります。<br>シネマサンシャインでのムビチケのご利用に関しましては<br>下記よりご確認ください。</p>
 					<a href="#howto">> ムビチケ券のご利用についてはこちら</a>
@@ -45,45 +45,45 @@ include("../../lib/require.php");
 				<div class="step">
 					<p class="step_title">STEP <span>1</span></p>
 					<p class="content_text">上映スケジュールから鑑賞したい作品を選択してください。</p>
-					<img src="../../sp/images/mvtk/mvtk_sp_1.gif" width="630" height="377" alt="">
+					<img src="../../sp/images/mvtk/mvtk_sp_1.gif" width="100%" alt="">
 				</div>
 				
 				<div class="step">
 					<p class="step_title">STEP <span>2</span></p>
 					<p class="content_text">鑑賞したい座席を選択し、利用規約の同意にチェックし「次へ」ボタンを押してください。</p>
-					<img src="../../sp/images/mvtk/mvtk_sp_2.gif" width="630" height="377" alt="">
+					<img src="../../sp/images/mvtk/mvtk_sp_2.gif" width="100%" alt="">
 				</div>
 
 				<div class="step">
 					<p class="step_title">STEP <span>3</span></p>
 					<p class="content_text">券種選択画面の上部にある、「ムビチケを利用する」ボタンを押してください。</p>
 					<p class="Lead_text red">※ムビチケを利用される方は、事前に「ムビチケ購入番号」と「ムビチケ暗証番号」をご用意ください。</p>
-					<img src="../../sp/images/mvtk/mvtk_sp_3.gif" width="630" height="377" alt="">
+					<img src="../../sp/images/mvtk/mvtk_sp_3.gif" width="100%" alt="">
 				</div>
 
 				<div class="step">
 					<p class="step_title">STEP <span>4</span></p>
 					<p class="content_text">利用するムビチケ券の「ムビチケ購入番号」と「ムビチケ暗証番号」を入力し、「認証する」を押してください。</p>
 					<p class="Lead_text red">※ムビチケ券によっては別途追加料金が発生する場合があります、差額が発生した場合は別途クレジットカードにて差額分をお支払いいただきます。</p>
-					<img src="../../sp/images/mvtk/mvtk_sp_4.gif" width="630" height="377" alt="">
+					<img src="../../sp/images/mvtk/mvtk_sp_4.gif" width="100%" alt="">
 				</div>
 
 				<div class="step">
 					<p class="step_title">STEP <span>5</span></p>
 					<p class="content_text">認証完了後、ムビチケ券の内容確認が表示されるので、問題なければ「券種選択へ戻る」ボタンを押してください。</p>
-					<img src="../../sp/images/mvtk/mvtk_sp_5.gif" width="630" height="377" alt="">
+					<img src="../../sp/images/mvtk/mvtk_sp_5.gif" width="100%" alt="">
 				</div>
 
 				<div class="step">
 					<p class="step_title">STEP <span>6</span></p>
 					<p class="content_text">再度、券種選択画面に戻ります。ムビチケ券は券種選択一覧に追加されていますので「券種を選択してください」ボタンを押して追加されたムビチケ券を選択し「次へ」ボタンを押してください。</p>
-					<img src="../../sp/images/mvtk/mvtk_sp_6.gif" width="630" height="377" alt="">
+					<img src="../../sp/images/mvtk/mvtk_sp_6.gif" width="100%" alt="">
 				</div>
 
 				<div class="step">
 					<p class="step_title">STEP <span>7</span></p>
 					<p class="content_text">後の手順は、画面に沿って手続きを進めていただきムビチケ券を利用してのオンラインチケット購入は完了となります。</p>
-					<img src="../../sp/images/mvtk/mvtk_sp_7.gif" width="630" height="377" alt="">
+					<img src="../../sp/images/mvtk/mvtk_sp_7.gif" width="100%" alt="">
 				</div>
 			</div>
 
@@ -96,15 +96,15 @@ include("../../lib/require.php");
 						<p class="content_text">【追加料金参考】</p>
 						<h4>| ムビチケ2D券をお持ちの方</h4>
 						<p class="content_text">3D上映を鑑賞する場合</p>
-						<img src="../../sp/images/mvtk/mvtk_sp_8.gif" width="360" alt="追加料金合計400円">
+						<img src="../../sp/images/mvtk/mvtk_sp_8.gif" width="100%" alt="追加料金合計400円">
 						<p class="content_text">4DX上映を鑑賞する場合</p>
-						<img src="../../sp/images/mvtk/mvtk_sp_9.gif" width="360" alt="追加料金合計1000円">
+						<img src="../../sp/images/mvtk/mvtk_sp_9.gif" width="100%" alt="追加料金合計1000円">
 					</div>
 
 					<div>
 						<h4>| ムビチケ3D券をお持ちの方</h4>
 						<p class="content_text">4DX上映を鑑賞する場合</p>
-						<img src="../../sp/images/mvtk/mvtk_sp_10.gif" width="360" alt="追加料金合計1000円">
+						<img src="../../sp/images/mvtk/mvtk_sp_10.gif" width="100%" alt="追加料金合計1000円">
 						<p class="Lead_text red">※ムビチケ3D券で2D上映の作品を鑑賞される場合、ムビチケ券のご利用は可能ですが差額分のご返金はございませんのでご注意ください。</p>
 					</div>
 				</div>
