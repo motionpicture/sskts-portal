8c50704d935b27a426dc9eb5369bd85d4b7d1cdd
sato <motionpicture34@motionpicture34-no-MacBook-Pro-2.local>
2017-07-07 14:03:25 +0900

    SSKTS-316 上映スケジュール検索結果 ：ボタン画像パス修正

--- sp/result.php
+++ sp/result.php
@@ -195,9 +195,9 @@ jQuery(function($){
 				if($time->available==6) {
 					echo '<td class="pfm_btn"></td>';
 				}else if ($time->available==1 || $time->available==4){
-					echo '<td class="pfm_btn"><img src="../../images/theater/btn_buyPtn1.png" height="30" alt="窓口"></td>';
+					echo '<td class="pfm_btn"><img src="./images/theater/btn_buyPtn1.png" height="30" alt="窓口"></td>';
 				}else if ($time->available==5) {
-					echo '<td class="pfm_btn"><img src="../../images/theater/btn_buyPtn5.png" height="30" alt="満席"></td>';
+					echo '<td class="pfm_btn"><img src="./images/theater/btn_buyPtn5.png" height="30" alt="満席"></td>';
 				}else {
                     echo '<td class="pfm_btn">';
 
