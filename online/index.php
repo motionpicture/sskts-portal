77edbe285ad70349ff59000ca4e0def72f55e3e4
sato <motionpicture34@motionpicture34-no-MacBook-Pro-2.local>
2017-07-31 17:14:48 +0900

    SSKTS-316 オンライン説明ページ文言追加(PC/SP)

--- online/index.php
+++ online/index.php
@@ -30,46 +30,23 @@ include("../lib/require.php");
 								<h3>オンラインチケット購入のメリット</h3>
 								<div class="merit">
 									<div class="merit-inner01">
-										<h4>
-											メリット１
-										</h4>
-										<p class="title thick">
-											スマートフォンでスマート入場!
-										</p>
-										<p class="text01 thick online_read_txt_center">
-											オンライン購入であれば、鑑賞日当日に発券機へ並ぶことなく入場用QRコードを<br> スマートフォンでご提示するだけでスムーズに入場可能です。
-										</p>
+										<h4>メリット１</h4>
+										<p class="title thick">スマートフォンでスマート入場!</p>
+										<p class="text01 thick online_read_txt_center">オンライン購入であれば、鑑賞日当日に発券機へ並ぶことなく入場用QRコードを<br> スマートフォンでご提示するだけでスムーズに入場可能です。</p>
 										<img src="../images/online/online_pc_01.gif" width="100%" alt="スマートフォンでスマート入場!">
-										<p class="thick">
-											スマート入場とは?
-										</p>
-										<p class="text03">
-											スマート入場とは、オンラインでチケットを購入していただきお手持ちのスマートフォンを使いQRコードで<br> 入場するペーパーレスな入場方法です。鑑賞日当日は劇場内発券機でのチケット発券が不要なため発券に掛かる
-											<br> 時間を気にすることなくスムーズに入場して頂けます。
-										</p>
-										<p class="thick">
-											スマート入場対応劇場
-										</p>
+										<p class="thick">スマート入場とは?</p>
+										<p class="text03">スマート入場とは、オンラインでチケットを購入していただきお手持ちのスマートフォンを使いQRコードで<br>
+											入場するペーパーレスな入場方法です。鑑賞日当日は劇場内発券機でのチケット発券が不要なため発券に掛かる<br>
+											時間を気にすることなくスムーズに入場して頂けます。</p>
+										<p class="thick">スマート入場対応劇場</p>
 										<div class="process">
-											<a href="../theater/aira/">
-												・シネマサンシャイン姶良
-											</a>
-											&nbsp;
-											<a href="../theater/kitajima/">
-												・シネマサンシャイン北島
-											</a>
+											<a href="../theater/aira/">・シネマサンシャイン姶良</a>&nbsp;<a href="../theater/kitajima/">・シネマサンシャイン北島</a>
 										</div>
 									</div>
 									<div class="merit-inner02">
-										<h4>
-											メリット2
-										</h4>
-										<p class="title thick">
-											上映日の2日前から予約可能！
-										</p>
-										<p class="text01 thick online_read_txt_center">
-											オンライン購入は観たい作品を上映日前に座席指定で予約可能です。
-										</p>
+										<h4>メリット2</h4>
+										<p class="title thick">上映日の2日前から予約可能！<br>※作品によって、予約開始日が異なる場合がございます。</p>
+										<p class="text01 thick online_read_txt_center">オンライン購入は観たい作品を上映日前に座席指定で予約可能です。</p>
 										<img src="../images/online/online_pc_02.gif" width="100%" alt="上映日の2日前から予約可能！">
 									</div>
 								</div>
@@ -92,18 +69,12 @@ include("../lib/require.php");
 										入場までの流れ
 									</h3>
 									<div class="admission-inner01">
-										<h4>
-											スマートフォンをお持ちの方
-										</h4>
+										<h4>スマートフォンをお持ちの方</h4>
 										<img src="../images/online/online_pc_04.gif" width="100%" alt="スマートフォンをお持ちの方">
 										<div class="title02">
-											<p class="text02">
-												<span>1. </span>オンラインでチケットを購入
-											</p>
+											<p class="text02"><span>1. </span>オンラインでチケットを購入</p>
 											<div class="process">
-												<p>
-													オンラインチケット購入の流れに沿ってチケットを購入。
-												</p>
+												<p>オンラインチケット購入の流れに沿ってチケットを購入。</p>
 											</div>
 										</div>
 										<div class="title02">
@@ -115,159 +86,103 @@ include("../lib/require.php");
 											<p class="text02">
 												<span>3. </span>入場の際に入場口スタッフにQRコードをご提示してご入場ください
 											</p>
-											<p class="text03">
-												※QRコードは、鑑賞時間の24時間前からチケット情報ページに表示されます。</p>
+											<p class="text03">※QRコードは、鑑賞時間の24時間前からチケット情報ページに表示されます。</p>
 											<div class="process">
-												<p>
-													<span class="thick01">手順 1</span><br> 購入完了メールに記載されているリンクもしくは、劇場ページにあるチケット照会バナーを押して チケット照会ページにアクセス。
-												</p>
+												<p><span class="thick01">手順 1</span><br> 購入完了メールに記載されているリンクもしくは、劇場ページにあるチケット照会バナーを押して チケット照会ページにアクセス。</p>
 											</div>
 											<div class="process">
-												<p>
-													<span class="thick01">手順 2</span><br> チケット情報照会画面にて予約番号と購入時に入力した電話番号を入力してチケット情報ページにアクセス。 掲載されているQRコードをスマートフォンの画面に表示してください。
-												</p>
+												<p><span class="thick01">手順 2</span><br> チケット情報照会画面にて予約番号と購入時に入力した電話番号を入力してチケット情報ページにアクセス。 掲載されているQRコードをスマートフォンの画面に表示してください。</p>
 											</div>
 											<div class="process">
-												<p>
-													<span class="thick01">手順 3</span><br> スマートフォンの画面で表示したQRコードを入場口スタッフにご提示していただくとご入場いただけます。
-												</p>
+												<p><span class="thick01">手順 3</span><br> スマートフォンの画面で表示したQRコードを入場口スタッフにご提示していただくとご入場いただけます。</p>
 											</div>
 										</div>
 										<div class=" bluezone">
-											<p>
-												オススメ<br> 入場用QRコードは鑑賞日時の24時間前から表示されるため、事前にチケット情報ページを「ホーム画面に 追加」していただくとスムーズにQRコードがご提示できるためオススメです。
-											</p>
+											<p>オススメ<br> 入場用QRコードは鑑賞日時の24時間前から表示されるため、事前にチケット情報ページを「ホーム画面に 追加」していただくとスムーズにQRコードがご提示できるためオススメです。</p>
 										</div>
-
 									</div>
+									
 									<div class="admission-inner02">
-										<h4>
-											スマートフォンをお持ちでない方
-										</h4>
+										<h4>スマートフォンをお持ちでない方</h4>
 										<img src="../images/online/online_pc_05.gif" width="100%" alt="スマートフォンをお持ちでない方">
 										<div class="title02">
-											<p class="text02">
-												<span>1.</span> オンラインでチケットを購入
-											</p>
+											<p class="text02"><span>1.</span> オンラインでチケットを購入</p>
 											<div class="process">
-												<p>
-													オンラインチケット購入の流れに沿ってチケットを購入。
-												</p>
+												<p>オンラインチケット購入の流れに沿ってチケットを購入。</p>
 											</div>
 										</div>
 										<div>
-											<p class="text02">
-												<span>2.</span> QRコードをご自宅で印刷
-											</p>
-											<p>
-												※劇場内に設置されている発券機からも発券可能です
-											</p>
+											<p class="text02"><span>2.</span> QRコードをご自宅で印刷</p>
+											<p>※劇場内に設置されている発券機からも発券可能です</p>
 											<div class="process">
-												<p>
-													<span class="thick01">手順 1</span><br> 購入完了メールに記載されているリンクもしくは、劇場ページにあるチケット照会バナーを押して チケット照会ページにアクセス。
-												</p>
+												<p><span class="thick01">手順 1</span><br> 購入完了メールに記載されているリンクもしくは、劇場ページにあるチケット照会バナーを押して チケット照会ページにアクセス。</p>
 											</div>
 											<div class="process">
-												<p>
-													<span class="thick01">手順 2</span><br> チケット情報照会画面にて予約番号と購入時に入力した電話番号を入力し、チケット情報ページにアクセス。 掲載されているQRコードをご自宅のプリンターで印刷しご持参ください。
-												</p>
+												<p><span class="thick01">手順 2</span><br> チケット情報照会画面にて予約番号と購入時に入力した電話番号を入力し、チケット情報ページにアクセス。 掲載されているQRコードをご自宅のプリンターで印刷しご持参ください。</p>
 											</div>
 										</div>
 										<div class=" bluezone">
-											<p>
-												オススメ<br> 入場用QRコードは鑑賞日時の24時間前から表示されるため、事前にチケット情報ページを印刷していただ くとスムーズに入場していただけます。
-											</p>
+											<p>オススメ<br> 入場用QRコードは鑑賞日時の24時間前から表示されるため、事前にチケット情報ページを印刷していただ くとスムーズに入場していただけます。</p>
 										</div>
+										<div class="title02"></div>
 										<div class="title02">
-										</div>
-										<div class="title02">
-											<p class="text02">
-												<span>3.</span> 鑑賞する劇場へご来場ください
-											</p>
+											<p class="text02"><span>3.</span> 鑑賞する劇場へご来場ください</p>
 											<div class="process">
-												<p>
-													ご自宅で印刷したQRコードを忘れずにご持参ください。<br> ※劇場内に設置されている発券機からもチケットを発券していただけます。
-												</p>
+												<p>ご自宅で印刷したQRコードを忘れずにご持参ください。<br> ※劇場内に設置されている発券機からもチケットを発券していただけます。</p>
 											</div>
-											<p class="redtext">
-												※発券機から発券する際は、予約番号と購入時に入力した電話番号が必要となります。
-											</p>
+											<p class="redtext">※発券機から発券する際は、予約番号と購入時に入力した電話番号が必要となります。</p>
 										</div>
 										<div>
-											<p class="text02">
-												<span>4.</span> 印刷したQRコード、または発券したチケットを入場時にご提示してご入場ください
-											</p>
+											<p class="text02"><span>4.</span> 印刷したQRコード、または発券したチケットを入場時にご提示してご入場ください</p>
 											<div class="process ather">
-												<p>
-													ご自宅で印刷していただいたQRコード、または劇場の発券機から発券したチケットを入場時間になりました ら入場口スタッフにご提示していただきますとご入場いただけます。
-												</p>
+												<p>ご自宅で印刷していただいたQRコード、または劇場の発券機から発券したチケットを入場時間になりました ら入場口スタッフにご提示していただきますとご入場いただけます。</p>
 											</div>
 										</div>
-										<div>
-										</div>
+									<div>
+								</div>
+							</div>
+
+							<div class="admission-inner03">
+								<h4>スマートフォン・プリンタをお持ちでない方</h4>
+								<img src="../images/online/online_pc_06.gif" width="100%" alt="スマートフォン・プリンタをお持ちでない方">
+								<div class="title02">
+									<p class="text02"><span>1.</span> オンラインでチケットを購入</p>
+									<div class="process">
+										<p>オンラインチケット購入の流れに沿ってチケットを購入。</p>
 									</div>
-									<div class="admission-inner03">
-										<h4>
-											スマートフォン・プリンタをお持ちでない方
-										</h4>
-										<img src="../images/online/online_pc_06.gif" width="100%" alt="スマートフォン・プリンタをお持ちでない方">
-										<div class="title02">
-											<p class="text02">
-												<span>1.</span> オンラインでチケットを購入
-											</p>
-											<div class="process">
-												<p>
-													オンラインチケット購入の流れに沿ってチケットを購入。
-												</p>
-											</div>
-										</div>
-										<div class="title02">
-											<p class="text02">
-												<span>2.</span> 鑑賞する劇場へご来場ください
-											</p>
-										</div>
-										<div class="title02">
-											<p class="text02">
-												<span>3.</span> 劇場の発券機にてチケットを発券
-											</p>
-											<p class="text03">
-												※発券機から発券する場合、予約番号と購入時に入力した電話番号が必要となります。
-											</p>
-											<div class="process">
-												<p>
-													予約番号と購入時に入力した電話番号をご用意の上、画面の指示に従って発券してください。
-												</p>
-											</div>
-											<p class="redtext">
-												※発券機から発券する際は、予約番号と購入時に入力した電話番号が必要となります。
-											</p>
-										</div>
-										<div class="title02">
-											<p class="text02">
-												<span>4.</span> 発券したチケットを入場口スタッフにご提示してご入場ください
-											</p>
-											<div class="process">
-												<p>
-													劇場で発券したチケットを入場口スタッフにご提示していただきますとご入場いただけます。
-												</p>
-											</div>
-										</div>
+								</div>
+								<div class="title02">
+									<p class="text02"><span>2.</span> 鑑賞する劇場へご来場ください</p>
+								</div>
+								<div class="title02">
+									<p class="text02"><span>3.</span> 劇場の発券機にてチケットを発券</p>
+									<p class="text03">※発券機から発券する場合、予約番号と購入時に入力した電話番号が必要となります。</p>
+									<div class="process">
+										<p>予約番号と購入時に入力した電話番号をご用意の上、画面の指示に従って発券してください。</p>
+									</div>
+									<p class="redtext">※発券機から発券する際は、予約番号と購入時に入力した電話番号が必要となります。</p>
+								</div>
+								<div class="title02">
+									<p class="text02"><span>4.</span> 発券したチケットを入場口スタッフにご提示してご入場ください</p>
+									<div class="process">
+										<p>劇場で発券したチケットを入場口スタッフにご提示していただきますとご入場いただけます。</p>
 									</div>
 								</div>
 							</div>
 						</div>
 					</div>
-
-					<!-- ↑修正する部分はここまで↑ -->
-
-					<?php getRightMenu(); ?>
 				</div>
 			</div>
-		</div>
-		<!-- #container end -->
+			<!-- ↑修正する部分はここまで↑ -->
 
-		<?php getFooter(); ?>
+			<?php getRightMenu(); ?>
+		</div>
 	</div>
+	</div>
+	<!-- #container end -->
+
+	<?php getFooter(); ?>
+</div>
 	<!-- #wrapper end -->
 </body>
 
